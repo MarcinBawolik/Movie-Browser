fb08c32f601a8efe935a142db018c165a8c2b446
PatrykSech <119355037+PatrykSech@users.noreply.github.com>
2023-04-12 22:16:21 +0200

    created media queries for a cast/crew tail

--- src/common/tail/index.js
+++ src/common/tail/index.js
@@ -1,11 +1,40 @@
-import { Container, Name, Photo, Role } from "./styled";
+import { Container, Name, Photo, Role, Person } from "./styled";
 import Poster from "../../images/poster.png";
 
 const Cast = () => (
+
+    // zamiast photo trzeba zrobic osobny div i przekazac mu background img, jesli nie ma nic to puste logo
     <Container>
-        <Photo src={Poster}></Photo>
+        <Person>
+            <Photo src={Poster}></Photo>
             <Name>Liu Yifei</Name>
             <Role>Mulan</Role>
+        </Person>
+        <Person>
+            <Photo src={Poster}></Photo>
+            <Name>Liu Yifei</Name>
+            <Role>Mulan</Role>
+        </Person>
+        <Person>
+            <Photo src={Poster}></Photo>
+            <Name>Liu Yifei</Name>
+            <Role>Mulan</Role>
+        </Person>
+        <Person>
+            <Photo src={Poster}></Photo>
+            <Name>Liu Yifei</Name>
+            <Role>Mulan</Role>
+        </Person>
+        <Person>
+            <Photo src={Poster}></Photo>
+            <Name>Liu Yifei</Name>
+            <Role>Mulan</Role>
+        </Person>
+        <Person>
+            <Photo src={Poster}></Photo>
+            <Name>Liu Yifei</Name>
+            <Role>Mulan</Role>
+        </Person>
     </Container>
 );
 
--- src/common/tail/styled.js
+++ src/common/tail/styled.js
@@ -1,6 +1,19 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
+    display: grid;
+    grid-template-columns: repeat(6, 1fr);
+    grid-gap: 24px;
+    filter: drop-shadow(0px 4px 12px rgba(186, 199, 213, 0.5));
+    border-radius: 5px;
+
+    @media (max-width: 320px) {
+    grid-template-columns: repeat(2, 1fr);
+    grid-gap: 16px;
+    }
+`
+
+export const Person = styled.div`
     display: flex;
     flex-direction: column;
     align-items: center;
@@ -8,13 +21,25 @@ export const Container = styled.div`
     height: 339px;
     background-color: white;
     justify-content: space-around;
-    filter: drop-shadow(0px 4px 12px rgba(186, 199, 213, 0.5));
+
+    @media (max-width: 320px) {
+    width: 136px;
+    height: 245px;
+    box-shadow: 0px 4px 12px rgba(186, 199, 213, 0.5);
+    border-radius: 5px;
+    }
 `
 export const Photo = styled.img`
     width: 176px;
     height: 231px;
     border-radius: 5px;
-    margin: 16px 16px 12px 16px
+    margin: 16px 16px 12px 16px;
+
+    @media (max-width: 320px) {
+    width: 120px;
+    height: 178px;
+    margin: 8px;
+    }
 `
 
 export const Name = styled.h3`
@@ -29,4 +54,9 @@ export const Role = styled.p`
     font-size: 18px;
     color: #7E839A;
     margin: 0 0 16px 0;
+
+    @media (max-width: 320px) {
+        font-size: 13px;
+        margin: 0 0 8px 0;
+    }
 `
