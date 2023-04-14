69e6c0de0ac7deb659f52bb92092e4eb0ce68143
MarcinBawolik <marcinbawolik2000@gmail.com>
2023-04-14 14:05:38 +0200

    demo error and loading

---
+++ src/PopularMovies/Content/Error/index.js
@@ -0,0 +1,5 @@
+export const Error = () => (
+    <div>
+        Ooops! Something went wrong...
+    </div>
+)
---
+++ src/PopularMovies/Content/Loading/index.js
@@ -0,0 +1,5 @@
+export const Loading = () => (
+    <div>
+        Please wait, Movies are being loaded...
+    </div>
+)
