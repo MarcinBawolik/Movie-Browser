b973006d1dfa0c00c77e3dd8aeb1d231966c1092
adrianplotka <115931816+adrianplotka@users.noreply.github.com>
2023-04-15 15:30:02 +0200

    create page scroller

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import { ActorProfile } from './components/ActorProfile';
 import { Error } from './components/Error';
+import { Pages } from './components/Pages';
 
 function App() {
   return (
     <>
       <Error/>
       <ActorProfile/>
+      <Pages/>
     </>
   );
 }
