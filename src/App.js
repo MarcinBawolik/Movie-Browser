e1f40be23eebc4b71646db139dfe1a0c83abbaf8
adrianplotka <115931816+adrianplotka@users.noreply.github.com>
2023-04-16 01:01:34 +0200

    add banner component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import { ActorProfile } from './components/ActorProfile';
+import { Banner } from './components/Banner';
 import { Error } from './components/Error';
 import { Pages } from './components/Pages';
 
 function App() {
   return (
     <>
+      <Banner/>
       <Error/>
       <ActorProfile/>
       <Pages/>
