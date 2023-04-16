767910142f0fba96b93e28cc17e7b4f71c70d979
adrianplotka <115931816+adrianplotka@users.noreply.github.com>
2023-04-16 19:38:17 +0200

    add media to page scroller

--- src/Pages/index.js
+++ src/Pages/index.js
@@ -10,43 +10,89 @@ import {
     Text,
 } from "./styled"
 
+import React, { useState, useEffect } from "react";
+
+
+
 
 export const Pages = () => {
+    const [width, setWidth] = useState(window.innerWidth);
+
+    useEffect(() => {
+        const handleResize = () => setWidth(window.innerWidth);
+        window.addEventListener("resize", handleResize);
+        return () => {
+            window.removeEventListener("resize", handleResize);
+        };
+    }, []);
+
     return (
         <>
-            <Wrapper>
-                <Container>
-                    <StyledTile>
-                        <VectorLeft />
-                        First
-                    </StyledTile>
-                </Container>
-                <Container>
-                    <StyledTile>
-                        <VectorLeft />
-                        Previous
-                    </StyledTile>
-                </Container>
-                <Page> 
-                    <Text>Page</Text>
-                    <Number>1</Number>
-                    <Text>of</Text>
-                    <Number>500</Number>
-                </Page>
-                <Container>
-                    <StyledTile>
-                        <VectorRight />
-                        Next
-                    </StyledTile>
-                </Container>
-                <Container>
-                    <StyledTile>
-                        <VectorRight />
-                        Last
-                    </StyledTile>
-                </Container>
-            </Wrapper>
+            <Wrapper>{width >= 767 ?
+                <>
+                    <Container>
+                        <StyledTile>
+                            <VectorLeft />
+                            First
+                        </StyledTile>
+                    </Container>
+                    <Container>
+                        <StyledTile>
+                            <VectorLeft />
+                            Previous
+                        </StyledTile>
+                    </Container>
+                    <Page>
+                        <Text>Page</Text>
+                        <Number>1</Number>
+                        <Text>of</Text>
+                        <Number>500</Number>
+                    </Page>
+                    <Container>
+                        <StyledTile>
+                            <VectorRight />
+                            Next
+                        </StyledTile>
+                    </Container>
+                    <Container>
+                        <StyledTile>
+                            <VectorRight />
+                            Last
+                        </StyledTile>
+                    </Container>
+                </>
+                :
+                <>
+                    <Container>
+                        <StyledTile>
+                            <VectorLeft />
+                            <VectorLeft />
+                        </StyledTile>
+                    </Container>
+                    <Container>
+                        <StyledTile>
+                            <VectorLeft />
+                        </StyledTile>
+                    </Container>
+                    <Page>
+                        <Text>Page</Text>
+                        <Number>1</Number>
+                        <Text>of</Text>
+                        <Number>500</Number>
+                    </Page>
+                    <Container>
+                        <StyledTile>
+                            <VectorRight />
+                        </StyledTile>
+                    </Container>
+                    <Container>
+                        <StyledTile>
+                            <VectorRight />
+                            <VectorRight />
+                        </StyledTile>
+                    </Container>
+                </>
+            }</Wrapper>;
         </>
-
     )
 }
