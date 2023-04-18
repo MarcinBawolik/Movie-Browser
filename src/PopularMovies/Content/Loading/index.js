d8ffc16b8abf079bd867cbf91ee172e0dc4b3aa4
MarcinBawolik <marcinbawolik2000@gmail.com>
2023-04-18 10:26:56 +0200

    moved to another component

--- src/PopularMovies/Content/Loading/index.js
+++
@@ -1,5 +0,0 @@
-export const Loading = () => (
-    <div>
-        Please wait, Movies are being loaded...
-    </div>
-)
--- src/components/NoResults/index.js
+++
@@ -1,9 +0,0 @@
-import { Text, Image } from "./styled";
-import noResults from "./images/noResults.png";
-
-export const NoReuslts = () => (
-  <>
-    <Text>Sorry, there are no results for “Muan”</Text>
-    <Image src={noResults} alt="" />
-  </>
-);
