612adc3c5c8b911cf1d246809b166f9024e4da52
PatrykSech <119355037+PatrykSech@users.noreply.github.com>
2023-04-19 20:59:26 +0200

    updates index.js

--- src/common/loader/index.js
+++ src/common/loader/index.js
@@ -1,13 +1,7 @@
 import { Load } from "./styled";
 
 const Loader = () => (
-<<<<<<< HEAD
         <Load></Load>
-);
-=======
-        <Load>
-        </Load>
 )
->>>>>>> main
 
 export default Loader;
