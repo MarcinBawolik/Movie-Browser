7b1048e7e162e1b2c6fc479657fb4a0504930db6
adrianplotka <115931816+adrianplotka@users.noreply.github.com>
2023-04-21 17:23:30 +0200

    improved responsiveness in ActorProfile

--- src/common/ActorProfile/index.js
+++ src/common/ActorProfile/index.js
@@ -1,27 +1,66 @@
+import { useMediaQuery } from "react-responsive";
 import actor from "./images/actor.png"
-import { Container, Content, Date, Details, Image, LeftText, Name, Place, RightText, Wrapper } from "./styled"
+import {
+    Container,
+    Content,
+    Date,
+    Details,
+    Image,
+    LeftText,
+    Name,
+    Place,
+    RightText,
+    StyledDiv,
+    Wrapper
+} from "./styled"
 
 export const ActorProfile = () => {
+    const isMobile = useMediaQuery({ maxWidth: 767 });
+
     return (
         <>
             <Container>
-                <Wrapper>
-                    <Image src={actor} alt="actor" />
-                    <Content>
-                        <Name>Liu Yifei</Name>
-                            <Date>
-                                <LeftText>Date of birth:</LeftText>
-                                <RightText>25.08.1987 </RightText>
-                            </Date>
-                            <Place>
-                                <LeftText>Place of birth:</LeftText>
-                                <RightText>Wuhan, Hubei, China</RightText>
-                            </Place>
-                        <Details>Liu Yifei was born in Wuhan, Hubei, Province of China on August 25th, 1987. She began modeling at the age of 8 and was trained in singing, dancing and the piano. Moving to the United States at 10 with her mother, Liu lived there for four years.</Details>
-                    </Content>
-                </Wrapper>
+                {isMobile ?
+                    <>
+                    <Wrapper>
+                        <Image src={actor} alt="actor" />
+                        <Content>
+                            <Name>Liu Yifei</Name>
+                            <StyledDiv>
+                                <Date>
+                                    <LeftText>Birth:</LeftText>
+                                    <RightText>25.08.1987 </RightText>
+                                </Date>
+                                <Place>
+                                    <LeftText>Place of birth:</LeftText>
+                                    <RightText>Wuhan, Hubei, China</RightText>
+                                </Place>
+                            </StyledDiv>
+                        </Content>
+                    </Wrapper>
+                    <Details>Liu Yifei was born in Wuhan, Hubei, Province of China on August 25th, 1987. She began modeling at the age of 8 and was trained in singing, dancing and the piano. Moving to the United States at 10 with her mother, Liu lived there for four years.</Details>
+                    </>
+                    :
+                    <Wrapper>
+                        <Image src={actor} alt="actor" />
+                        <Content>
+                            <Name>Liu Yifei</Name>
+                            <StyledDiv>
+                                <Date>
+                                    <LeftText>Date of birth:</LeftText>
+                                    <RightText>25.08.1987 </RightText>
+                                </Date>
+                                <Place>
+                                    <LeftText>Place of birth:</LeftText>
+                                    <RightText>Wuhan, Hubei, China</RightText>
+                                </Place>
+                            </StyledDiv>
+                            <Details>Liu Yifei was born in Wuhan, Hubei, Province of China on August 25th, 1987. She began modeling at the age of 8 and was trained in singing, dancing and the piano. Moving to the United States at 10 with her mother, Liu lived there for four years.</Details>
+                        </Content>
+                    </Wrapper>
+                }
             </Container>
         </>
     )
 
-}
+};
--- src/common/ActorProfile/styled.js
+++ src/common/ActorProfile/styled.js
@@ -5,22 +5,57 @@ export const Container = styled.div`
     max-width: 1326px;
     margin: auto;
     margin-top: 56px;
-` 
+` ;
 export const Wrapper = styled.div`
     display: flex;
     flex-direction: row;
     align-items: flex-start;
     padding: 40px;
     gap: 40px;
-`
+
+    @media (max-width: 767px) {
+    padding: 28px;
+    gap: 28px;
+  }
+
+    @media (max-width: 480px) {
+    padding: 16px;
+    gap: 16px;
+  }
+`;
 
 export const Image = styled.img`
     border-radius: 5px;
-`
+
+    @media (max-width: 1023px) {
+    width: 40%;
+  }
+
+  @media (max-width: 767px) {
+    width: 45%;
+  }
+
+  @media (max-width: 480px) {
+   width: 45%;
+  }
+
+  @media (max-width: 320px) {
+    height: 183px;
+    width: 130px;
+  }
+`;
 
 export const Content = styled.div`
+    display: grid;
+    gap: 24px;
 
-`
+    @media (max-width: 480px) {
+    gap: 16px;
+  }
+`;
+
+export const StyledDiv = styled.div`
+`;
 
 export const Name = styled.h1`
     margin-top: 8px;
@@ -29,17 +64,37 @@ export const Name = styled.h1`
     font-weight: 600;
     font-size: 36px;
     line-height: 120%;
-`
+
+    @media (max-width: 767px) {
+    font-size: 24px;
+  }
+
+    @media (max-width: 480px) {
+    font-weight: 500;
+    font-size: 14px;
+    line-height: 130%;
+  }
+`;
 
 export const Date = styled.div`
     display: flex;
-    margin: 0px;
-`
+    gap: 10px;
+   
+    @media(max-width: 767px) {
+    gap: 4px;
+  }
+`;
 
 export const Place = styled.p`
     display: flex;
+    gap: 10px;
     margin: 8px 0px 0px 0px;
-`
+    
+    @media (max-width: 767px) {
+    flex-wrap: wrap;
+    gap: 4px;
+  }
+`;
 
 export const LeftText = styled.p`
     color: #74788B;
@@ -48,8 +103,16 @@ export const LeftText = styled.p`
     font-weight: 400;
     font-size: 18px;
     line-height: 120%;
-    margin: 0px;
-`
+
+    @media (max-width: 767px) {
+    font-size: 16px;
+  }
+
+    @media (max-width: 480px) {
+    font-size: 12px;
+    
+  }
+`;
 
 export const RightText = styled.p`
     color: #000000;
@@ -58,8 +121,18 @@ export const RightText = styled.p`
     font-weight: 500;
     font-size: 18px;
     line-height: 120%;
-    margin: 0px 0px 0px 10px; 
-`
+
+    @media (max-width: 767px) {
+    font-size: 16px;
+  }
+    
+
+    @media (max-width: 480px) {
+    font-weight: 400;
+    font-size: 12px;    
+  }
+
+`;
 
 export const Details = styled.p`
     font-family: 'Poppins';
@@ -67,6 +140,16 @@ export const Details = styled.p`
     font-weight: 400;
     font-size: 20px;
     line-height: 160%;
-
     text-align: justify;
-`
+    position: relative;
+
+    @media (max-width: 767px) {
+    font-size: 20px;
+    padding: 0px 16px;
+  }
+
+  @media (max-width: 480px) {
+    font-size: 14px;
+    padding: 0px 16px;
+  }
+`;
