9a049cea284c95d603e00db472ca42e2756fcf73
MarcinBawolik <marcinbawolik2000@gmail.com>
2023-04-24 12:50:00 +0200

    added Cast and Crew to MovieDetails

--- src/features/Movies/MovieDetails/index.js
+++ src/features/Movies/MovieDetails/index.js
@@ -1,20 +1,31 @@
 import { Banner } from "../../../common/Banner";
+import { MovieMemberTile } from "../../../common/MovieMemberTile";
 import { MovieDetailsTile } from "../../../common/MovieDetailsTile";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { getMovieDetails } from "../moviesAPI";
-import { Wrapper } from "./styled";
+import { getMovieDetails, getMovieCredits } from "../moviesAPI";
+import { MovieDetailsWrapper, Wrapper, Header, List } from "./styled";
+import noPicture from "../../../images/noPicture.png";
 
 export const MovieDetails = () => {
   const { id } = useParams();
   const [movieDetails, setMovieDetails] = useState(null);
+  const [movieCredits, setMovieCredits] = useState(null);
 
   useEffect(() => {
-    const fetchData = async () => {
+    const fetchDetailsData = async () => {
       const data = await getMovieDetails({ id });
       setMovieDetails(data);
     };
-    fetchData();
+    fetchDetailsData();
+  }, [id]);
+
+  useEffect(() => {
+    const fetchCreditsData = async () => {
+      const data = await getMovieCredits({ id });
+      setMovieCredits(data);
+    };
+    fetchCreditsData();
   }, [id]);
 
   return (
@@ -28,7 +39,7 @@ export const MovieDetails = () => {
             votes={movieDetails.vote_count}
             rate={movieDetails.vote_average.toFixed(1)}
           />
-          <Wrapper>
+          <MovieDetailsWrapper>
             <MovieDetailsTile
               id={id}
               imageSrc={`https://image.tmdb.org/t/p/w300/${movieDetails.poster_path}`}
@@ -44,9 +55,49 @@ export const MovieDetails = () => {
               content={movieDetails.overview}
               content2={movieDetails.overview}
             />
-          </Wrapper>
+          </MovieDetailsWrapper>
         </>
       ) : null}
+      <Wrapper>
+        <Header>Cast</Header>
+        <List>
+          {movieCredits &&
+            movieCredits.cast &&
+            movieCredits.cast.map((cast) => (
+              <MovieMemberTile
+                as="li"
+                id={cast.id}
+                name={cast.character}
+                role={cast.name}
+                photo={
+                  cast.profile_path
+                    ? `https://image.tmdb.org/t/p/w185/${cast.profile_path}`
+                    : noPicture
+                }
+              />
+            ))}
+        </List>
+      </Wrapper>
+      <Wrapper>
+        <Header>Crew</Header>
+        <List>
+          {movieCredits &&
+            movieCredits.crew &&
+            movieCredits.crew.map((crew) => (
+              <MovieMemberTile
+                as="li"
+                id={crew.id}
+                role={crew.job}
+                name={crew.name}
+                photo={
+                  crew.profile_path
+                    ? `https://image.tmdb.org/t/p/w185/${crew.profile_path}`
+                    : noPicture 
+                }
+              />
+            ))}
+        </List>
+      </Wrapper>
     </>
   );
 };
