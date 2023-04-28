d0cbd1ffe2ad596fc020fc6100c116ea3349e9f8
adrianplotka <115931816+adrianplotka@users.noreply.github.com>
2023-04-28 15:32:18 +0200

    download crew and cast from API

---
+++ src/common/PersonDetailsTile/index.js
@@ -0,0 +1,43 @@
+import {
+    Tile,
+    Image,
+    Year,
+    Rate,
+    Content,
+    Tags,
+    Tag,
+    Votes,
+    VotesWrapper,
+    Star,
+    Title,
+  } from "./styled";
+  
+  export const PersonDetailsTile = ({
+    id,
+    imageSrc,
+    altText,
+    title,
+    year,
+    genreList,
+    rate,
+    votes,
+  }) => (
+    <Tile key={id}>
+      <Image src={imageSrc} alt={altText} />
+      <Content>
+        <Title>{title}</Title>
+        <Year>{year}</Year>
+        <Tags>
+          {genreList &&
+            Array.isArray(genreList) &&
+            genreList.map((genre) => <Tag key={genre.id}>{genre.name}</Tag>)}
+        </Tags>
+        <VotesWrapper>
+          <Star />
+          <Rate>{rate}</Rate>
+          <Votes>{votes}</Votes>
+        </VotesWrapper>
+      </Content>
+    </Tile>
+  );
+  
