be514d51b2364374780f29f83fa9a139c5b3a35b
adrianplotka <115931816+adrianplotka@users.noreply.github.com>
2023-05-30 11:02:54 +0200

    change API in people nad movie to QueryClient. Ad queryKey

--- src/features/Movies/moviesAPI.js
+++ src/features/Movies/moviesAPI.js
@@ -14,7 +14,9 @@ export async function getPopularMovies() {
     }
   }
 
-  export const getMovieDetails = async ({ id }) => {
+  export const getMovieDetails = async ({ queryKey  }) => {
+    const [, { id }] = queryKey;
+
     try {
       const response = await fetch(`${API_URL}movie/${id}?api_key=${API_KEY}`);
       if (!response.ok) {
@@ -27,15 +29,15 @@ export async function getPopularMovies() {
     }
   }
   
-  export const getMovieCredits = async ({ id }) => {
-    try {
-      const response = await fetch(`${API_URL}movie/${id}/credits?api_key=${API_KEY}`);
-      if (!response.ok) {
-        throw new Error("An error occurred while fetching movie credits.");
-      }
-      const data = await response.json();
-      return data;
-    } catch (error) {
-      console.error(error);
+  export const getMovieCredits = async ({ queryKey }) => {
+    const [, { id }] = queryKey;
+
+    const response = await fetch(
+      `${API_URL}movie/${id}/credits?api_key=${API_KEY}`);
+  
+    if (!response.ok) {
+      throw new Error(response.statusText);
     }
+  
+    return await response.json();
   }
