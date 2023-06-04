c5fafe9fb53acc0d4b76ab10d2fec58322bfed54
MarcinBawolik <marcinbawolik2000@gmail.com>
2023-06-04 23:29:56 +0200

    added query to NoResults component

--- src/common/NoResults/index.js
+++ src/common/NoResults/index.js
@@ -1,9 +1,14 @@
 import { Text, Image } from "./styled";
 import noResults from "./images/noResults.png";
-
-export const NoReuslts = () => (
-  <>
-    <Text>Sorry, there are no results for “Muan”</Text>
+import { useLocation } from "react-router-dom";
+import { searchQueryParamName } from "../../searchQueryParamName";
+export const NoResults = () => {
+  const location = useLocation();
+  const query = new URLSearchParams(location.search).get(searchQueryParamName);
+  return (
+    <>
+    <Text>Sorry, there are no results for “{query}”</Text>
     <Image src={noResults} alt="" />
   </>
-);
+  )
+};
