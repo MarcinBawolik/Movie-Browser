5e1f369c19a0b3a96b49b7b646658e575ebf5491
MarcinBawolik <marcinbawolik2000@gmail.com>
2023-06-27 00:20:11 +0200

    changed path in Error component

--- src/common/Error/index.js
+++ src/common/Error/index.js
@@ -10,7 +10,7 @@ export const Error = () => {
             <WrapperError>
                 <HeaderError>Ooops! Something went wrong... </HeaderError>
                 <InfoError>Please check your network connection<br />and try again</InfoError>
-                <StyledLink to={`/movies/`}>
+                <StyledLink to={`/people/popular-people`}>
                     <ButtonLink>Back to home page</ButtonLink>
                 </StyledLink>
             </WrapperError>
