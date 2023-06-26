f1dd31ff9f58680f066c2d420018247d3143a2ca
MarcinBawolik <marcinbawolik2000@gmail.com>
2023-06-27 00:23:22 +0200

    popular people and movies loading all results

--- src/features/Movies/PopularMovies/index.js
+++ src/features/Movies/PopularMovies/index.js
@@ -1,12 +1,109 @@
 import { Container } from "./styled";
 import { useDispatch, useSelector } from "react-redux";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import {
   fetchPopularMoviesLoading,
   selectPopularMovies,
   selectPopularMoviesStatus,
 } from "../moviesSlice";
 import { Content } from "./Content";
+import { Header, MovieList, StyledLink } from "./Content/Success/styled";
+import { Genres } from "../../../getMovieGenres";
+import { MovieTile } from "../../../common/MovieTile";
+import Pages from "../../../common/Pages";
+
+import { getPopularMovies } from "../moviesAPI";
+import Loader from "../../../common/Loader";
+import { Error } from "../../../common/Error";
+
+const MovieComponent = () => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const resultsPerPage = 20;
+
+  const handlePageChange = (newPage) => {
+    setCurrentPage(newPage);
+    setMovies([]);
+  };
+
+  const fetchMovies = async () => {
+    setLoading(true);
+    setError(null);
+  
+    try {
+      const results = await getPopularMovies(currentPage);
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+  
+      setMovies((prevMovies) => [...prevMovies, ...results]);
+    } catch (error) {
+      console.error(error);
+      setError("Wystąpił błąd podczas pobierania filmów.");
+    }
+  
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchMovies();
+  }, [currentPage]);
+
+  if (loading) {
+    return <Loader />;
+  }
+
+  if (error) {
+    return <Error />;
+  }
+  return (
+    <>
+      <Header>Popular Movies</Header>
+      <Genres>
+        {({ genres }) => (
+          <MovieList>
+            {movies.map(
+              ({
+                id,
+                title,
+                poster_path,
+                release_date,
+                vote_average,
+                vote_count,
+                genre_ids,
+              }) => (
+                <>
+                  <StyledLink to={`/movies/movies/${id}`}>
+                    <MovieTile
+                      key={id}
+                      as="li"
+                      id={id}
+                      title={title}
+                      imageSrc={`https://image.tmdb.org/t/p/w500/${poster_path}`}
+                      altText={title}
+                      year={release_date.slice(0, 4)}
+                      genreList={genres.filter((genre) =>
+                        genre_ids.includes(genre.id)
+                      )}
+                      rate={vote_average}
+                      votes={vote_count}
+                    />
+                  </StyledLink>
+                </>
+              )
+            )}
+          </MovieList>
+        )}
+      </Genres>
+      <Pages
+        totalPages={500}
+        currentPage={currentPage}
+        onPageChange={handlePageChange}
+      />
+    </>
+  );
+};
 
 export const PopularMovies = () => {
   const dispatch = useDispatch();
@@ -20,7 +117,7 @@ export const PopularMovies = () => {
   return (
     <>
       <Container>
-        <Content status={moviesStatus} movies={movies} />
+        <MovieComponent />
       </Container>
     </>
   );
--- src/features/People/PopularPeople/Content/Success/index.js
+++ src/features/People/PopularPeople/Content/Success/index.js
@@ -1,46 +1,79 @@
 import { Person, PeopleList, Name, Image, StyledLink, Header } from "./styled";
-import Pages from "../../../../../common/Pages"
-import { useState } from "react";
+import Pages from "../../../../../common/Pages";
+import { useState, useEffect } from "react";
+import { getPopularPeople } from "../../../peopleAPI";
+import Loader from "../../../../../common/Loader";
+import { Error } from "../../../../../common/Error";
+import noPicture from "../../../../../images/noPicture.png";
 
-export const Success = ({ people }) => {
-      const [currentPage, setCurrentPage] = useState(1);
-      const [resultsPerPage] = useState(18);
+export const Success = () => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const [people, setPeople] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
 
-      const indexOfLastResult = currentPage * resultsPerPage;
-      const indexOfFirstResult = indexOfLastResult - resultsPerPage;
-      const currentResults = people.slice(indexOfFirstResult, indexOfLastResult);
+  const resultsPerPage = 18;
 
-      const handlePageChange = (newPage) => {
-        setCurrentPage(newPage);
-      };
+  const handlePageChange = (newPage) => {
+    setCurrentPage(newPage);
+    setPeople([]);
+  };
 
-      return (
-        <>
-        <Header>Popular People</Header>
-          <PeopleList>
-            {currentResults.map(({ id,
-              name,
-              profile_path
-            }) => (
-              <>
-                <StyledLink to={`/people/people/${id}`}>
-                  <Person key={id}>
-                    <Image
-                      src={`https://image.tmdb.org/t/p/w185/${profile_path}`}
-                      alt={name}
-                    />
-                    <Name>{name}</Name>
-                  </Person>
-                </StyledLink>
-              </>
-            ))}
-          </PeopleList>
-          <Pages
-           totalResults={people.length}
-           resultsPerPage={resultsPerPage}
-           currentPage={currentPage}
-           onPageChange={handlePageChange}
-          />
-        </>
-      );
-    };
+  const fetchPeople = async () => {
+    setLoading(true);
+    setError(null);
+
+    try {
+      const results = await getPopularPeople(currentPage);
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+
+      setPeople((prevPeople) => [...prevPeople, ...results]);
+    } catch (error) {
+      console.error(error);
+      setError("Wystąpił błąd podczas pobierania postaci.");
+    }
+
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchPeople();
+  }, [currentPage]);
+
+  if (loading) {
+    return <Loader />;
+  }
+
+  if (error) {
+    return <Error />;
+  }
+  return (
+    <>
+      <Header>Popular People</Header>
+      <PeopleList>
+        {people.map(({ id, name, profile_path }) => (
+          <>
+            <StyledLink to={`/people/people/${id}`}>
+              <Person key={id}>
+                <Image
+                  src={
+                    profile_path
+                      ? `https://image.tmdb.org/t/p/w185/${profile_path}`
+                      : noPicture
+                  }
+                  alt={name}
+                />
+                <Name>{name}</Name>
+              </Person>
+            </StyledLink>
+          </>
+        ))}
+      </PeopleList>
+      <Pages
+        totalPages={500}
+        currentPage={currentPage}
+        onPageChange={handlePageChange}
+      />
+    </>
+  );
+};
